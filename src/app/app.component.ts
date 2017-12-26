d5fa16586ed16ad30a746854b34a753033e23845
Aakash Thakur <thakurakash321@gmail.com>
2017-12-27 00:45:40 +0530

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
 import { Component } from '@angular/core';
+import { trigger, style, transition, animate, group }
+    from '@angular/animations';
+import { Subject } from 'rxjs/Subject';
+import { DataService } from "./services/data.service";
+import * as _ from "lodash";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DataService],
+  animations: [
+  trigger('itemAnim', [
+    transition(':enter', [
+      style({transform: 'translateX(-100%)'}),
+      animate(350)
+    ]),
+    transition(':leave', [
+      group([
+        animate('0.2s ease', style({
+          transform: 'translate(150px,25px)'
+        })),
+        animate('0.5s 0.2s ease', style({
+          opacity: 0
+        }))
+      ])
+    ])
+  ])
+]
 })
+
 export class AppComponent {
   title = 'app';
+  active = true;
+  color = true;
+  items:any = [];
+  results: Object;
+  searchTerm$ = new Subject<string>();
+
+
+  constructor(private dataService: DataService){
+    this.dataService.search(this.searchTerm$)
+      .subscribe(results => {
+        this.results = results.results;
+      });
+  }
+
+
+  fruits = ['bananas', 'oranges', 'strawberries', 'kiwis'];
+
+ngOnInit () {
+  console.log(_.chunk(this.fruits, 2));
+}
+
+  myStyles = {
+'background-color': 'lime',
+'font-size': '20px',
+'font-weight': 'bold'
+}
+
+myClasses={
+  active:this.active,
+  color:this.color
+}
+
+val=20;
+
+addItem(value){
+  this.items.push(value);
+}
+
+removeItem(value){
+  this.items.splice(value,1);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes }   from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SettingsComponent } from './components/settings/settings.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'settings', component: SettingsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
